067d3c99ce3884961125e4680f48b9106d26532a
Vanderlei K Silva <vanderleiksilva@gmail.com>
2020-07-15 17:04:11 -0300

    Implementation Users Exception

---
+++ src/main/java/com/project/spring/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.project.spring.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.project.spring.services.exceptions.ResourceNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException ex, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
